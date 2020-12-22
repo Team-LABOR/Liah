2322bac58041f8b974ddfcd6c969b9e7b83bb350
CwhiteKJ <dw696969@naver.com>
2020-12-22 19:37:26 +0900

    Docs:  add code source

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # LIAH
 
 _제작 중..._
+
+코드베이스: [Wonderbot](https://github.com/wonderlandpark/wonderbot/blob/develop/src/index.js)
--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  client: "mysql",
-  connection: {
-    host: "122.34.222.210",
-    user: "root",
-    password: "steel6900",
-    database: "liah",
-  },
-}
