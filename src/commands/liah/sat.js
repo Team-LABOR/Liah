ca377d597953f7cfd3e61f78411048b307ccb6cb
kyjkyj080115 <kyj080115@gmail.com>
2020-12-28 15:44:50 +0900

    feat: add sat command

---
+++ src/commands/liah/sat.js
@@ -0,0 +1,35 @@
+module.exports.execute = async (
+  client,
+  message,
+  locale,
+  embed,
+  _tools,
+  knex
+) => {
+  message.channel.send(locale.commands.sat.title).then((m) => {
+    var t = new Date()
+    var theDate = new Date(2021, 11, 03)
+    var diffDate = theDate - t
+    var result = Math.ceil(diffDate / (60 * 1000 * 60 * 24))
+
+    knex("users")
+      .select("*")
+      .limit(1)
+      .then(() => {
+        embed.addField(
+          locale.commands.sat.title,
+          locale.commands.sat.return.bind({
+            day: result,
+          })
+        )
+
+        m.edit({ content: locale.commands.sat.title, embed })
+      })
+  })
+}
+module.exports.props = {
+  name: "수능",
+  perms: "general",
+  alias: ["수능", "CollegeScholasticAbilityTest"],
+  args: [],
+}
