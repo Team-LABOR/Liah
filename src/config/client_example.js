9cb03be1ad456b2ba31d7174173765664ddaa9f4
kyjkyj080115 <kyj080115@gmail.com>
2020-12-28 15:28:56 +0900

    Docs: add code source

---
+++ src/config/client_example.js
@@ -0,0 +1,24 @@
+module.exports = {
+  webhook: {
+    error: {
+      id: "",
+      token: "",
+    },
+  },
+
+  //github: 'push github link',
+  app: {
+    messageCacheMaxSize: 50,
+    presence: { activity: { name: "리아" } },
+  },
+
+  shard: {
+    totalShard: 2,
+    respawn: true,
+    token: "",
+  },
+
+  token: "",
+  prefix: "리아야 ",
+  owners: [],
+}
