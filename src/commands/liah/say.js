4d500fa51297dea0b1294202c378138184b5dc90
kyjkyj080115 <kyj080115@gmail.com>
2020-12-28 15:45:40 +0900

    src/locale/ko.js

---
+++ src/commands/liah/say.js
@@ -0,0 +1,33 @@
+const { prefix } = require("../../config/client")
+module.exports.execute = async (
+  client,
+  message,
+  locale,
+  embed,
+  _tools,
+  knex
+) => {
+  message.channel.send(locale.commands.say.wait).then((m) => {
+    const text = message.content.slice(`${prefix}말해 `.length)
+
+    knex("users")
+      .select("*")
+      .limit(1)
+      .then(() => {
+        embed.addField(
+          locale.commands.say.bird,
+          locale.commands.say.return.bind({
+            say: text,
+          })
+        )
+
+        m.edit({ content: locale.commands.say.title, embed })
+      })
+  })
+}
+module.exports.props = {
+  name: "말해",
+  perms: "general",
+  alias: ["말해"],
+  args: [],
+}
