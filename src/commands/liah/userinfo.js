82d9855b4980ac5addded961be4f8eede405bba8
CwhiteKJ <dw696969@naver.com>
2020-12-28 11:41:18 +0900

    fix: userinfo command

--- src/commands/liah/userinfo.js
+++ src/commands/liah/userinfo.js
@@ -1,44 +1,46 @@
 module.exports.execute = async (
-    client,
-    message,
-    locale,
-    embed,
-    _tools,
-    knex
-  ) => {
-    message.channel.send(locale.commands.userinfo.wait).then((m) => {
-        let clientstatus;
-        
-        let status = message.author.presence.clientStatus
+  _client,
+  message,
+  locale,
+  embed,
+  _tools,
+  knex
+) => {
+  message.channel.send(locale.commands.userinfo.wait).then((m) => {
+    let _status = message.author.presence.clientStatus
+    let clientstatus = []
 
-for(let key in status) {
-    let statusObj = status[key]
-    clientstatus = (key+": " +statusObj)
-}
+    for (let key in _status) {
+      key = key
+        .replace("mobile", "모바일")
+        .replace("desktop", "컴퓨터")
+        .replace("web", "웹")
+      clientstatus.push(key)
+    }
 
-      knex("users")
-        .select("*")
-        .limit(1)
-        .then(() => {
-            embed.addField(
-            locale.commands.userinfo.title,
-            locale.commands.userinfo.message.bind({
-              name: message.author.username,
+    knex("users")
+      .select("*")
+      .limit(1)
+      .then(() => {
+        embed.addField(
+          locale.commands.userinfo.title,
+          locale.commands.userinfo.message.bind({
+            name: message.author.username,
             tag: message.author.discriminator,
             id: message.author.id,
-            avatar: message.author.avatarURL({format: "png"}),
-            status: clientstatus,
-            })
-          )
-        
-          m.edit({ content: locale.commands.userinfo.title, embed })
-        })
-    })
-  }
-  module.exports.props = {
-    name: "유저정보",
-    perms: "general",
-    alias: ["유저정보", "userinfo"],
-    args: [],
-  }
-  
+            avatar: message.author.avatarURL({ format: "png" }),
+            status: message.author.presence.activities[0],
+            client: clientstatus.join(", "),
+          })
+        )
+
+        m.edit({ content: locale.commands.userinfo.title, embed })
+      })
+  })
+}
+module.exports.props = {
+  name: "유저정보",
+  perms: "general",
+  alias: ["유저정보", "userinfo"],
+  args: [],
+}
