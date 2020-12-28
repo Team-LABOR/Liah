4ca98eed974aced2ef297eed9d1b08a08b1fcab5
kyjkyj080115 <kyj080115@gmail.com>
2020-12-28 16:16:22 +0900

    feat: add clear commad

---
+++ src/commands/liah/userinfo.js
@@ -0,0 +1,48 @@
+module.exports.execute = async (
+  _client,
+  message,
+  locale,
+  embed,
+  _tools,
+  knex
+) => {
+  message.channel.send(locale.commands.userinfo.wait).then((m) => {
+    let _status = message.author.presence.clientStatus
+    let clientstatus = []
+
+    for (let key in _status) {
+      key = key
+        .replace("mobile", "모바일")
+        .replace("desktop", "컴퓨터")
+        .replace("web", "웹")
+      clientstatus.push(key)
+    }
+
+    let activity = message.author.presence.activities[0]
+    let a = activity.name === "Custom Status" ? activity.state : activity.name
+    knex("users")
+      .select("*")
+      .limit(1)
+      .then(() => {
+        embed.addField(
+          locale.commands.userinfo.title,
+          locale.commands.userinfo.message.bind({
+            name: message.author.username,
+            tag: message.author.discriminator,
+            id: message.author.id,
+            avatar: message.author.avatarURL({ format: "png" }),
+            status: a,
+            client: clientstatus.join(", "),
+          })
+        )
+
+        m.edit({ content: locale.commands.userinfo.title, embed })
+      })
+  })
+}
+module.exports.props = {
+  name: "유저정보",
+  perms: "general",
+  alias: ["유저정보", "userinfo"],
+  args: [],
+}
