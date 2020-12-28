96c908b382c51d00b4325a203de00d6813350910
CwhiteKJ <dw696969@naver.com>
2020-12-28 11:41:54 +0900

    코드 스타일 수정 및 명령어 등록

--- src/commands/liah/corona.js
+++ src/commands/liah/corona.js
@@ -1,32 +1,31 @@
 module.exports.execute = async (
-    client,
-    message,
-    locale,
-    embed,
-    _tools,
-    knex
-  ) => {
-    message.channel.send(locale.commands.corona.wait).then((m) => {
-        const request = require("request")
+  client,
+  message,
+  locale,
+  embed,
+  _tools,
+  knex
+) => {
+  message.channel.send(locale.commands.corona.wait).then((m) => {
+    const request = require("request")
 
+    let url = "https://apiv2.corona-live.com/stats.json"
+    request(url, (error, response, body) => {
+      let overview = JSON.parse(response.body).overview
+      overview = {
+        total_confirmed_person: overview.confirmed[0], // 총 확진자수
+        yesterday_confirmed_person: overview.confirmed[1], // 어제 확진자수
 
-        let url = "https://apiv2.corona-live.com/stats.json"
-        request(url, (error, response, body) => {
-            let overview = JSON.parse(response.body).overview;
-            overview = {
-                total_confirmed_person: overview.confirmed[0], // 총 확진자수
-                yesterday_confirmed_person: overview.confirmed[1], // 어제 확진자수
-        
-                current_confirmed_person: overview.current[0], // 현재 확진자수
-                current_confirmed_person_diff: overview.current[1], // (어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
-            }
-    
-        knex("users")
+        current_confirmed_person: overview.current[0], // 현재 확진자수
+        current_confirmed_person_diff: overview.current[1], // (어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+      }
+
+      knex("users")
         .select("*")
         .limit(1)
         .then(() => {
-            embed.addField(
-              locale.commands.corona.sub,
+          embed.addField(
+            locale.commands.corona.sub,
             locale.commands.corona.return.bind({
               allcorona: overview.total_confirmed_person,
               yesterdaycorona: overview.yesterday_confirmed_person,
@@ -34,17 +33,15 @@ module.exports.execute = async (
               todayminusyesterday: overview.current_confirmed_person_diff,
             })
           )
-          
+
           m.edit({ content: locale.commands.corona.title, embed })
         })
-        })
-  
     })
-  }
-  module.exports.props = {
-    name: "코로나",
-    perms: "general",
-    alias: ["코로나", "corona"],
-    args: [],
-  }
-  
+  })
+}
+module.exports.props = {
+  name: "코로나",
+  perms: "general",
+  alias: ["코로나", "corona"],
+  args: [],
+}
--- src/commands/liah/hangangriver.js
+++ src/commands/liah/hangangriver.js
@@ -1,38 +1,35 @@
-const axios = require('axios');
+const axios = require("axios")
 
-        module.exports.execute = async (
-            client,
-            message,
-            locale,
-            embed,
-            _tools,
-            knex
-          ) => {
-            
-        axios.get('http://hangang.dkserver.wo.tc/').then(function (x) {
-            message.channel.send(locale.commands.hangangriver.wait).then((m) => {
-              
-              knex("users")
-                .select("*")
-                .limit(1)
-                .then(() => {
-                    embed.addField(
-                    locale.commands.hangangriver.setTitle,
-                    locale.commands.hangangriver.return.bind({
-                    river:(x.data.temp),
-                    })
-                  )
-          
-                  m.edit({ content: locale.commands.hangangriver.title, embed })
-                })
+module.exports.execute = async (
+  client,
+  message,
+  locale,
+  embed,
+  _tools,
+  knex
+) => {
+  axios.get("http://hangang.dkserver.wo.tc/").then(function (x) {
+    message.channel.send(locale.commands.hangangriver.wait).then((m) => {
+      knex("users")
+        .select("*")
+        .limit(1)
+        .then(() => {
+          embed.addField(
+            locale.commands.hangangriver.setTitle,
+            locale.commands.hangangriver.return.bind({
+              river: x.data.temp,
             })
-          })
-          }
-        
-          module.exports.props = {
-            name: "한강",
-            perms: "general",
-            alias: ["한강", "hanganriver"],
-            args: [],
-          }
-          
+          )
+
+          m.edit({ content: locale.commands.hangangriver.title, embed })
+        })
+    })
+  })
+}
+
+module.exports.props = {
+  name: "한강",
+  perms: "general",
+  alias: ["한강", "hanganriver"],
+  args: [],
+}
